3b6251187da7467ed3890aa3c18294a525be2819
Sostene Munezero Bagira <bagirasostene@gmail.com>
2022-02-19 19:07:58 +0200

    ADD: functionality of pomodo

--- src/Reducers/pomodoReducer.js
+++ src/Reducers/pomodoReducer.js
@@ -1,13 +1,99 @@
-export const initialState={
-    breakLength:5,
-    sessionLength:25,
-    timer:25*60,
-}
-function pomodoReducer(state, action) {
+export const initialState = {
+  breakLength: 5,
+  sessionLength: 25,
+  timer: 25 * 60,
+  isSession: true,
+  isRunning: false,
+};
+const INCREMENT_BREAK_LENGTH = "INCREMENT_BREAK_LENGTH";
+const DECREMENT_BREAK_LENGTH = "DECREMENT_BREAK_LENGTH";
+const INCREMENT_SESSION_LENGTH = "INCREMENT_SESSION_LENGTH";
+const DECREMENT_SESSION_LENGTH = "DECREMENT_SESSION_LENGTH";
+const DECREMENT_TIMER = "DECREMENT_TIMER";
+const RESET_EVERYTHING = "RESET_EVERYTHING";
+const TOGGLE_SESSION_STATUS = "TOGGLE_SESSION_STATUS";
+function pomodoReducer(state = initialState, action) {
   switch (action.type) {
+    case INCREMENT_BREAK_LENGTH:
+      if (state.breakLength < 60 && state.breakLength)
+        return Object.assign({}, state, {
+          breakLength: state.breakLength + 1,
+          timer:
+            (!state.isSession && (state.breakLength + 1) * 60) || state.timer,
+        });
+      return state;
+    case DECREMENT_BREAK_LENGTH:
+      if (state.breakLength > 1)
+        return Object.assign({}, state, {
+          breakLength: state.breakLength - 1,
+          timer:
+            (!state.isSession && (state.breakLength - 1) * 60) || state.timer,
+        });
+      return state;
+
+    case INCREMENT_SESSION_LENGTH:
+      if (state.sessionLength < 60)
+        return Object.assign({}, state, {
+          sessionLength: state.sessionLength + 1,
+          timer:
+            (state.isSession && (state.sessionLength + 1) * 60) || state.timer,
+        });
+      return state;
+    case DECREMENT_SESSION_LENGTH:
+      if (state.sessionLength > 1)
+        return Object.assign({}, state, {
+          sessionLength: state.sessionLength - 1,
+          timer:
+            (state.isSession && (state.sessionLength - 1) * 60) || state.timer,
+        });
+      return state;
+    case DECREMENT_TIMER:
+      if (state.timer > 0)
+        return Object.assign({}, state, { timer: state.timer - 1 });
+      else {
+        return Object.assign({}, state, {
+          timer: state.isSession
+            ? state.breakLength * 60
+            : state.sessionLength * 60,
+          isSession: !state.isSession,
+        });
+      }
+
+    case TOGGLE_SESSION_STATUS:
+      return Object.assign({}, state, { isRunning: !state.isRunning });
+    case RESET_EVERYTHING:
+      return initialState;
     default:
       return state;
   }
 }
+export const incrementBreakLength = () => ({
+  type: INCREMENT_BREAK_LENGTH,
+});
+export const decrementBreakLength = () => ({
+  type: DECREMENT_BREAK_LENGTH,
+});
+export const incrementSessionLength = () => ({
+  type: INCREMENT_SESSION_LENGTH,
+});
+export const decrementSessionLength = () => ({
+  type: DECREMENT_SESSION_LENGTH,
+});
+export const decrementTimer = () => ({
+  type: DECREMENT_TIMER,
+});
+export const toggleSessionStatus = () => ({
+  type: TOGGLE_SESSION_STATUS,
+});
+export const resetEverything = () => ({
+  type: RESET_EVERYTHING,
+});
 
+export const getMinutesAndSeconds = (time) => {
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  return `${minutes / 10 < 1 ? "0" + minutes : minutes}:${
+    seconds / 10 < 1 ? "0" + seconds : seconds
+  }`;
+};
 export default pomodoReducer;
--- src/pages/Pomodo.js
+++ src/pages/Pomodo.js
@@ -1,9 +1,139 @@
+import { useCallback, useEffect, useReducer, useRef, useState } from "react";
+import pomodoReducer, {
+  initialState,
+  decrementBreakLength,
+  decrementSessionLength,
+  decrementTimer,
+  getMinutesAndSeconds,
+  incrementBreakLength,
+  incrementSessionLength,
+  toggleSessionStatus,
+  resetEverything,
+} from "../Reducers/pomodoReducer";
 
 function Pomodo() {
+  const [pomodo, dispatch] = useReducer(pomodoReducer, initialState);
+  const [intervalFunctionId, setIntervalFunctionId] = useState(null);
+  const [
+    decrBreakLen,
+    decrSessionLen,
+    decrTimer,
+    incrBreakLen,
+    incrSessionLen,
+    toggleStatus,
+    reset,
+  ] = [
+    decrementBreakLength,
+    decrementSessionLength,
+    decrementTimer,
+    incrementBreakLength,
+    incrementSessionLength,
+    toggleSessionStatus,
+    resetEverything,
+  ].map((action) => () => dispatch(action()));
+  const resetAll = useCallback(() => {
+    console.log(intervalFunctionId);
+    audioElement.current.pause();
+    audioElement.current.currentTime = 0;
+    if (pomodo.isRunning) {
+      clearInterval(intervalFunctionId);
+      setIntervalFunctionId(null);
+      toggleStatus();
+    }
 
-  return <div>
+    reset();
+  }, [setIntervalFunctionId, intervalFunctionId, toggleStatus, reset, pomodo]);
+  const audioElement = useRef();
+  const toggleTime = useCallback(() => {
+    if (!intervalFunctionId) {
+      let timeInterval = setInterval(decrTimer, 10);
+      setIntervalFunctionId(timeInterval);
+    } else {
+      clearInterval(intervalFunctionId);
+      setIntervalFunctionId(null);
+    }
+    toggleStatus();
+  }, [decrTimer, setIntervalFunctionId, intervalFunctionId, toggleStatus]);
+  useEffect(() => {
+    pomodo.timer === 0 && audioElement.current.play();
+  }, [pomodo]);
+  return (
+    <div className="flex--column" id="pomodo">
+      <h1 className="text-gradient-1 text--center"> 25 + 5 Clock</h1>
+      <div className="flex--row">
+        <div className="flex--column w-6 flex-center">
+          <h2 className="text--center" id="break-label">
+            Break Length
+          </h2>
+          <div className="flex--row flex--center">
+            <button
+              className="button--styled m-4"
+              onClick={decrBreakLen}
+              id="break-decrement"
+              disabled={pomodo.isRunning}
+            >
+              -
+            </button>
+            <p id="break-length">{pomodo.breakLength}</p>
+            <button
+              className="button--styled m-4"
+              onClick={incrBreakLen}
+              id="break-increment"
+              disabled={pomodo.isRunning}
+            >
+              +
+            </button>
+          </div>
+        </div>
+        <div className="flex--column w-6">
+          <h2 className="text--center" id="session-label">
+            Session Length
+          </h2>
+          <div className="flex--row flex--center">
+            <button
+              className="button--styled m-4"
+              onClick={decrSessionLen}
+              id="session-decrement"
+              disabled={pomodo.isRunning}
+            >
+              -
+            </button>
+            <p id="session-length">{pomodo.sessionLength}</p>
+            <button
+              className="button--styled m-4"
+              onClick={incrSessionLen}
+              id="session-increment"
+              disabled={pomodo.isRunning}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="text--center">
+        <h1 id="timer-label"> {pomodo.isSession ? "Session" : "Break"}</h1>
+        <h1 id="time-left"> {getMinutesAndSeconds(pomodo.timer)}</h1>
+      </div>
+      <div className="flex--row flex--center">
+        <button
+          className="button--styled m-4 m-4"
+          onClick={toggleTime}
+          id="start_stop"
+        >
+          start_stop
+        </button>
+        <button
+          className="button--styled m-4 m-4"
+          onClick={resetAll}
+          id="reset"
+        >
+          Reset
+        </button>
+      </div>
 
-  </div>;
+      <audio id="beep" src="/beep.wav" ref={audioElement}></audio>
+    </div>
+  );
 }
 
 export default Pomodo;
